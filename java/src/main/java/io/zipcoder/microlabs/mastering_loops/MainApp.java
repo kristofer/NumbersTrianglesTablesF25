dd3e85414c097e535c62a3c4d7da86e13ce493e0
Kristofer <kris@youngers.org>
2025-11-04 10:43:18 -0500

    works in vscode

--- java/src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
+++ java/src/main/java/io/zipcoder/microlabs/mastering_loops/MainApp.java
@@ -1,11 +1,10 @@
 package io.zipcoder.microlabs.mastering_loops;
 
-import java.util.Scanner;
-
 /**
  * Created by leon on 1/31/18.
  */
 public class MainApp {
     public static void main(String[] args) {
+        System.out.println("Hello, Fall25!");
     }
 }
